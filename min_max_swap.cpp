3b1828fd2a024c1b4762ebec4e5f25f86b42b5bf
erab2r <shkera213@gmail.com>
2025-05-26 00:51:45 +0600

    5th

---
+++ min_max_swap.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int a,b;
+    cin >> a >> b;
+    cout << min({5,3,4,90,1}) << endl;
+    cout << max({5,3,4,90,1}) << endl;
+    swap(a,b);
+    cout <<a <<' ' << b << endl;
+    
+
+    return 0;
+
+} 
