c55edb51f1c0066633713b4874a0d22761dec2de
erab2r <shkera213@gmail.com>
2025-05-25 17:09:35 +0600

    first commit

---
+++ print.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cin >> x ;
+    cout << x <<endl;
+     char c = 'A';
+     int ascii = c;
+     cout << ascii << endl;
+
+    return 0;
+}
